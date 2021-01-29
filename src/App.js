04a328d573e1547113213f5a59e79938c707bdc7
zth.laptop <thzhao@mail.ustc.edu.cn>
2021-01-29 21:57:59 +0800

    origion-files

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import Video from "./Video";
+import "./App.css";
+import data from './videos.json';
+function App() {
+  const [videos, setVideos] = useState([{data:[]}]);
+
+  useEffect(() => {
+            setVideos(data);
+        }, []);
+  return (
+    // BEM
+    <div className="app">
+      <div className="app__videos">
+        {videos.map(
+          ({
+               id: id, username: username,
+               description: description,hashtags,soundName, videoURL,likeCount,  commentCount, shareCount, liked }) => {
+              return (
+                  <Video key={id}
+                         url={videoURL}
+                         channel={username}
+                         song={soundName}
+                         likes={likeCount}
+                         messages={commentCount}
+                         description={description}
+                         shares={shareCount}
+                  />
+              );
+          }
+        )}
+      </div>
+    </div>
+  );
+}
+export default App;
