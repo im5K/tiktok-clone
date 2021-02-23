c05500982437341a1cdbe3c1d08152bee0a84f16
zth.laptop <thzhao@mail.ustc.edu.cn>
2021-02-23 21:43:52 +0800

    good

--- src/Video.js
+++ src/Video.js
@@ -2,57 +2,28 @@ import React, {useEffect, useRef, useState} from "react";
 import VideoFooter from "./VideoFooter";
 import VideoSidebar from "./VideoSidebar";
 import "./Video.css";
-import {nanoid} from 'nanoid';
+
 function Video({ url, channel, description, song, likes, messages, shares }) {
-  const [playing, setPlaying] = useState(true);
-  const [loop, setLoop] = useState(0);
-  const [mouse, setMouse] = useState(false);
-  const videoRef = useRef(nanoid());
-  const divvideoRef = useRef(nanoid());
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
 
   const onVideoPress = () => {
     if (playing) {
-        stopVideo()
+      videoRef.current.pause();
       setPlaying(false);
     } else {
-        startVideo()
-        setLoop(loop+1);
+      videoRef.current.play();
       setPlaying(true);
     }
   };
-  function stopVideo(){
-      videoRef.current.pause();
-  }
-  function startVideo(){
 
-        videoRef.current.play();
-    }
-    function mouseState(flag){
-      setMouse(flag);
-    }
-    useEffect(()=>{
-
-        if(loop===0&&mouse){
-            console.log("初始化播放")
-            startVideo()
-            setLoop(loop+1);
-            setPlaying(true);
-        }else if(!mouse){
-            stopVideo();
-            setPlaying(false);
-        }
-    },[loop,mouse])
-    // console.log(divvideoRef.current.);
   return (
-    <div ref = {divvideoRef} onMouseEnter={()=>mouseState(true)}
-        onMouseLeave={()=>mouseState(false)} className="video">
+    <div className="video">
       <video
-          ref={videoRef}
         className="video__player"
         loop
         onClick={onVideoPress}
-          // onMouseEnter={handleMouse(true)}
-          // onMouseLeave={handleMouse(false)}
+        ref={videoRef}
         src={url}
       ></video>
       <VideoFooter channel={channel} description={description} song={song} />
